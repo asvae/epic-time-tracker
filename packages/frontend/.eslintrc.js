56b3ed5c70f801fcef0aa8f9e018b8207d86f88a
Asva <ontrew@gmail.com>
2019-04-03 15:09:54 +0300

    chore: tweak linter

--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -10,7 +10,9 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    'curly': 'error',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
